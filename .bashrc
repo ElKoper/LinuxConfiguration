985911f5a4fd7d1a634bab57e51961e21f208591
ElKoper <hwt365@gmail.com>
2015-12-19 03:43:18 +0100

    Simple bash and git configuration.

---
+++ .bashrc
@@ -0,0 +1,44 @@
+# Needed for vim schemes 
+export TERM=xterm-256color
+
+# Use colored grep always
+alias grep="grep --color"
+
+# Some git aliases 
+alias gitc="git checkout"
+alias gitb="git branch"
+alias gitr="git rebase"
+alias gitri="git rebase -i"
+alias gitd="git diff"
+alias gits="git status"
+
+gitp() {
+   branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+   git push origin $branch 
+}
+
+gitpf() {
+    branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+    git push origin :$branch
+    git push origin $branch
+}
+
+# Enable tab completion in git aliases
+[ -f ~/other/git-completion.bash ] && source ~/other/git-completion.bash
+__git_complete gitc _git_checkout
+__git_complete gitb _git_branch
+__git_complete gitr _git_rebase
+__git_complete gitri _git_rebase
+__git_complete gitd _git_diff
+
+
+# Remove trailing whitespaces
+trailcut(){
+  sed -i 's/[ \t]*$//' "$1"
+}
+
+# Change prompt
+[ -f ~/other/bash_prompt.sh ] && source ~/other/bash_prompt.sh
+
+# Load work only bashrc
+[ -f ~/.bashrc_intel ] && source ~/.bashrc_intel
---
+++ other/bash_prompt.sh
@@ -0,0 +1,60 @@
+# Configure bash prompt
+# Regular Colors
+Black='\[\e[0;30m\]'        # Black
+Red='\[\e[0;31m\]'          # Red
+Green='\[\e[0;32m\]'        # Green
+Yellow='\[\e[0;33m\]'       # Yellow
+Blue='\[\e[0;34m\]'         # Blue
+Purple='\[\e[0;35m\]'       # Purple
+Cyan='\[\e[0;36m\]'         # Cyan
+White='\[\e[0;37m\]'        # White
+Light_Gray='\[\033[0;37m\]'
+
+# Bold
+BBlack='\[\e[1;30m\]'       # Black
+BRed='\[\e[1;31m\]'         # Red
+BGreen='\[\e[1;32m\]'       # Green
+BYellow='\[\e[1;33m\]'      # Yellow
+BBlue='\[\e[1;34m\]'        # Blue
+BPurple='\[\e[1;35m\]'      # Purple
+BCyan='\[\e[1;36m\]'        # Cyan
+BWhite='\[\e[1;37m\]'       # White
+BLight_Gray='\[\033[1;37m\]'
+
+# High Intensity
+IBlack='\[\e[0;90m\]'       # Black
+IRed='\[\e[0;91m\]'         # Red
+IGreen='\[\e[0;92m\]'       # Green
+IYellow='\[\e[0;93m\]'      # Yellow
+IBlue='\[\e[0;94m\]'        # Blue
+IPurple='\[\e[0;95m\]'      # Purple
+ICyan='\[\e[0;96m\]'        # Cyan
+IWhite='\[\e[0;97m\]'       # White
+
+# Bold High Intensity
+BIBlack='\[\e[1;90m\]'      # Black
+BIRed='\[\e[1;91m\]'        # Red
+BIGreen='\[\e[1;92m\]'      # Green
+BIYellow='\[\e[1;93m\]'     # Yellow
+BIBlue='\[\e[1;94m\]'       # Blue
+BIPurple='\[\e[1;95m\]'     # Purple
+BICyan='\[\e[1;96m\]'       # Cyan
+BIWhite='\[\e[1;97m\]'      # White
+
+# Reset colors
+NONE="\[\e[0m\]"
+
+# Run git-prompt.sh script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GIT_PS1_SHOWDIRTYSTATE=1
+source $DIR/git-prompt.sh
+
+# Prompt components
+ps1_git='$(if command -v __git_ps1 &>/dev/null ; then __git_ps1 "\[\e[0;36m\](%s)\[\e[m\]\n\n"; fi)'
+ps1_time="[$Yellow$(date +%k:%M)$NONE]"
+ps1_dir="[$Green\w$NONE]"
+ps1_trail="\n$Green\$$NONE "
+
+# prompt
+export PS1="${ps1_git}${ps1_time}${ps1_dir}${ps1_trail}"
+
