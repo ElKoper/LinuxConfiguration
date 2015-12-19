610bb64c2bb7d3f42899ac88ef208df55dc094aa
ElKoper <hwt365@gmail.com>
2015-12-19 04:09:08 +0100

    Add installation script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# TODO: Option to replace anyway
+echo "Existing files will not be replaced."
+
+# vim configuration
+[ -f ~/.vimrc ] && echo "File ~/.vimrc exists." || ln -s $DIR/source/.vimrc ~/.vimrc
+[ -d ~/.vim ] && echo "Directory ~/.vim exists." || ln -sd $DIR/source/.vim ~/.vim 
+
+# bash configuration
+[ -f ~/.bashrc ] && echo "File ~/.bashrc exists." || ln -s $DIR/source/.bashrc ~/.bashrc
+[ -d ~/other ] && echo "Directory ~/git exists." || ln -sd $DIR/source/other ~/other 
+
