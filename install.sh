7d9fdc471c0b3ad8db148a5cfe14ff2f94b3f155
Lukasz Koper <lukasz.koper@intel.com>
2015-12-21 11:50:42 +0100

    Instalation script: add git submodule update

--- install.sh
+++ install.sh
@@ -7,9 +7,12 @@ echo "Existing files will not be replaced."
 
 # vim configuration
 [ -f ~/.vimrc ] && echo "File ~/.vimrc exists." || ln -s $DIR/source/.vimrc ~/.vimrc
-[ -d ~/.vim ] && echo "Directory ~/.vim exists." || ln -sd $DIR/source/.vim ~/.vim 
+[ -d ~/.vim ] && echo "Directory ~/.vim exists." || ln -sd $DIR/source/.vim ~/.vim
 
 # bash configuration
 [ -f ~/.bashrc ] && echo "File ~/.bashrc exists." || ln -s $DIR/source/.bashrc ~/.bashrc
-[ -d ~/other ] && echo "Directory ~/git exists." || ln -sd $DIR/source/other ~/other 
+[ -d ~/other ] && echo "Directory ~/other exists." || ln -sd $DIR/source/other ~/other
+
+# update submodules
+cd $DIR && git submodule update --init
 
