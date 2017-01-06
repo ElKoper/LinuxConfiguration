72c81abc1de9f70193178af82bb74989dca3f004
lkoper <lukasz.koper@intel.com>
2017-01-06 15:38:37 +0100

    Change prompt

--- source/other/bash_prompt.sh
+++ source/other/bash_prompt.sh
@@ -1,48 +1,25 @@
 # Configure bash prompt
-# Regular Colors
-Black='\[\e[0;30m\]'        # Black
-Red='\[\e[0;31m\]'          # Red
-Green='\[\e[0;32m\]'        # Green
-Yellow='\[\e[0;33m\]'       # Yellow
-Blue='\[\e[0;34m\]'         # Blue
-Purple='\[\e[0;35m\]'       # Purple
-Cyan='\[\e[0;36m\]'         # Cyan
-White='\[\e[0;37m\]'        # White
-Light_Gray='\[\033[0;37m\]'
 
-# Bold
-BBlack='\[\e[1;30m\]'       # Black
-BRed='\[\e[1;31m\]'         # Red
-BGreen='\[\e[1;32m\]'       # Green
-BYellow='\[\e[1;33m\]'      # Yellow
-BBlue='\[\e[1;34m\]'        # Blue
-BPurple='\[\e[1;35m\]'      # Purple
-BCyan='\[\e[1;36m\]'        # Cyan
-BWhite='\[\e[1;37m\]'       # White
-BLight_Gray='\[\033[1;37m\]'
+# Colors
+Black='\[\e[0;30m\]'
+Red='\[\e[0;31m\]'
+Green='\[\e[0;32m\]'
+Yellow='\[\e[0;33m\]'
+Blue='\[\e[0;34m\]'
+Purple='\[\e[0;35m\]'
+Cyan='\[\e[0;36m\]'
+White='\[\e[0;37m\]'
+None='\[\e[0m\]'
 
-# High Intensity
-IBlack='\[\e[0;90m\]'       # Black
-IRed='\[\e[0;91m\]'         # Red
-IGreen='\[\e[0;92m\]'       # Green
-IYellow='\[\e[0;93m\]'      # Yellow
-IBlue='\[\e[0;94m\]'        # Blue
-IPurple='\[\e[0;95m\]'      # Purple
-ICyan='\[\e[0;96m\]'        # Cyan
-IWhite='\[\e[0;97m\]'       # White
-
-# Bold High Intensity
-BIBlack='\[\e[1;90m\]'      # Black
-BIRed='\[\e[1;91m\]'        # Red
-BIGreen='\[\e[1;92m\]'      # Green
-BIYellow='\[\e[1;93m\]'     # Yellow
-BIBlue='\[\e[1;94m\]'       # Blue
-BIPurple='\[\e[1;95m\]'     # Purple
-BICyan='\[\e[1;96m\]'       # Cyan
-BIWhite='\[\e[1;97m\]'      # White
-
-# Reset colors
-NONE="\[\e[0m\]"
+_Black='\e[0;30m'
+_Red='\e[0;31m'
+_Green='\e[0;32m'
+_Yellow='\e[0;33m'
+_Blue='\e[0;34m'
+_Purple='\e[0;35m'
+_Cyan='\e[0;36m'
+_White='\e[0;37m'
+_None='\e[0m'
 
 # Run git-prompt.sh script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -50,11 +27,13 @@ GIT_PS1_SHOWDIRTYSTATE=1
 source $DIR/git-prompt.sh
 
 # Prompt components
-ps1_git='$(if command -v __git_ps1 &>/dev/null ; then __git_ps1 "\[\e[0;36m\](%s)\[\e[m\]\n\n"; fi)'
-ps1_time="[$Yellow$(date +%k:%M)$NONE]"
-ps1_dir="[$Green\w$NONE]"
-ps1_trail="\n$Green\$$NONE "
+ps1_exit_code() {
+    [[ 0 == "$1" || 130 == "$1" ]] || printf -- "[${_Red}Error code: $1${_None}]\n${_None}"
+}
+ps1_git='$(if command -v __git_ps1 &>/dev/null ; then __git_ps1 "[${_Cyan}%s${_None}]\n\n"; fi)'
+ps1_time="[${Yellow}$(date +%k:%M)${None}]"
+ps1_dir="[${Green}\w${None}]"
+ps1_trail="\n${Green}\$${None} "
 
-# prompt
-export PS1="${ps1_git}${ps1_time}${ps1_dir}${ps1_trail}"
-
+# Export prompt
+export PS1='$(ps1_exit_code $?)'"${ps1_git}${ps1_time}${ps1_dir}${ps1_trail}"
