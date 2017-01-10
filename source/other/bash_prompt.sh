d0217df74d2b3db4cd38af0462a8e59fd8c6f2b5
lkoper <lukasz.koper@intel.com>
2017-01-10 13:00:50 +0100

    Improved bash prompt

--- source/other/bash_prompt.sh
+++ source/other/bash_prompt.sh
@@ -1,6 +1,6 @@
 # Configure bash prompt
 
-# Colors
+# Colors with \[ ... \] enclosure (bash ignore them when computing the length of the prompt)
 Black='\[\e[0;30m\]'
 Red='\[\e[0;31m\]'
 Green='\[\e[0;32m\]'
@@ -8,9 +8,12 @@ Yellow='\[\e[0;33m\]'
 Blue='\[\e[0;34m\]'
 Purple='\[\e[0;35m\]'
 Cyan='\[\e[0;36m\]'
-White='\[\e[0;37m\]'
+LGray='\[\e[0;37m\]'
+DGray='\[\e[0;90m\]'
+White='\[\e[0;97m\]'
 None='\[\e[0m\]'
 
+# Colors
 _Black='\e[0;30m'
 _Red='\e[0;31m'
 _Green='\e[0;32m'
@@ -18,7 +21,9 @@ _Yellow='\e[0;33m'
 _Blue='\e[0;34m'
 _Purple='\e[0;35m'
 _Cyan='\e[0;36m'
-_White='\e[0;37m'
+_LGray='\e[0;37m'
+_DGray='\e[0;90m'
+_White='\e[0;97m'
 _None='\e[0m'
 
 # Run git-prompt.sh script
@@ -26,14 +31,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GIT_PS1_SHOWDIRTYSTATE=1
 source $DIR/git-prompt.sh
 
-# Prompt components
-ps1_exit_code() {
-    [[ 0 == "$1" || 130 == "$1" ]] || printf -- "[${_Red}Error code: $1${_None}]\n${_None}"
-}
-ps1_git='$(if command -v __git_ps1 &>/dev/null ; then __git_ps1 "[${_Cyan}%s${_None}]\n\n"; fi)'
-ps1_time="[${Yellow}$(date +%k:%M)${None}]"
-ps1_dir="[${Green}\w${None}]"
-ps1_trail="\n${Green}\$${None} "
+# Prompt command
+__prompt_command() {
+    local exit_code="$?"
+    PS1="\n"
 
-# Export prompt
-export PS1='$(ps1_exit_code $?)'"${ps1_git}${ps1_time}${ps1_dir}${ps1_trail}"
+    # git prompt
+    PS1+='$(if command -v __git_ps1 &>/dev/null ; then __git_ps1 "${_Cyan}%s${_None}\n${_None}"; fi)'
+    # time
+    PS1+="${Yellow}$(date +%k:%M)${None} "
+    # current directory
+    PS1+="${Green}\w${None} "
+    # error code
+    [[ 0 == "$exit_code" ]] && PS1+="${DGray}" || PS1+="${Red}" ; PS1+="$exit_code${None}"
+    # trail
+    PS1+="\n${Green}\$${None} "
+}
+
+# Export prompt command
+export PROMPT_COMMAND=__prompt_command
