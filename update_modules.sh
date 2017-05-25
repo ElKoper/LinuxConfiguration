ccbaa47ad472eb681618d23d7f9c04f548637da7
Lukasz Koper <lukasz.koper@intel.com>
2017-05-25 14:04:15 +0200

    Do not clone submodules' repos with every update.

--- update_modules.sh
+++ update_modules.sh
@@ -2,19 +2,33 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR
 
-# Remove old files
-rm -rf ./source/.vim/bundle/nerdtree/
-rm -rf ./source/.vim/bundle/tagbar/
-rm -rf ./source/.vim/bundle/vim-airline/
-rm -rf ./source/.vim/bundle/vim-colors-solarized/
-rm -rf ./source/.vim/bundle/vim-fugitive/
-rm -rf ./source/.vim/bundle/vim-gitgutter/
+function update_submodule {
+    USER=$1
+    PROJECT=$2
+    BRANCH=$3
+
+    # Remove old files
+    rm -rf ./source/.vim/bundle/${PROJECT}
+
+    # Dowanload new files from github
+    wget -O /tmp/${PROJECT}-${BRANCH}.zip \
+            https://github.com/${USER}/${PROJECT}/archive/${BRANCH}.zip
+    unzip /tmp/${PROJECT}-${BRANCH}.zip -d /tmp
+    mv /tmp/${PROJECT}-${BRANCH} ./source/.vim/bundle/${PROJECT}
+
+    # Remove unneded .zip file
+    rm /tmp/${PROJECT}-${BRANCH}.zip
+}
+
+# Remove files created by old update script
+rm -rf .git/modules/
 
 # Update submodules
-git submodule update --init --recursive
-
-# Remove unneded repos files
-rm -rf ./.git/modules
-rm `find . -name .git -type f`
+update_submodule   scrooloose    nerdtree               master
+update_submodule   majutsushi    tagbar                 master
+update_submodule   bling         vim-airline            master
+update_submodule   altercation   vim-colors-solarized   master
+update_submodule   tpope         vim-fugitive           master
+update_submodule   airblade      vim-gitgutter          master
 
 popd
